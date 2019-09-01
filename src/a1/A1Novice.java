3a063a4150ebe413ee76b16ae60789c20d48bd49
sayoni <sayoni@sayonis-mbp.wireless-1x.unc.edu>
2019-08-31 20:14:26 -0400

    Fixed code to make it functional

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,41 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// create int to store total number of customers
+		
+		int customernumber = scan.nextInt();
+		
+		// create for loop to store each customer name in array
+		double price = 0;
+		double totalprice = 0;
+		double subtotalprice = 0;
+		int numberofitems = 0;
+		int totalitems = 0;
+		String firstname = new String();
+		String lastname = new String();
+		String itemname = new String();
+		
+		for (int i=0; i < customernumber; i++) {
+			firstname = scan.next();
+			lastname = scan.next();
+			totalitems = scan.nextInt();
+			for (int j=0; j < totalitems; j++) {
+				numberofitems = scan.nextInt();
+				itemname = scan.next();
+				price = scan.nextDouble();
+				subtotalprice = price * numberofitems;
+				totalprice = totalprice + subtotalprice;
+				/* for (int k=0; k <numberofitems; k++) {
+					totalprice = subtotalprice;
+					totalprice = Double.sum(subtotalprice, subtotalprice);					
+				} */
+			}
+			String firstletter = String.valueOf(firstname.charAt(0));
+			System.out.println(firstletter + ". " + lastname + ": " + totalprice);
+		}
+		
+		// everything is done so close scanner
+		scan.close();
 		
 	}
 }
