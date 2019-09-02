77924553250b36614999eb2593716b69ef7b1495
sayoni <sayoni@sayonis-mbp.wireless-1x.unc.edu>
2019-09-01 22:04:43 -0400

    trying to implement a bunch of loops

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,49 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int inventorynumber = scan.nextInt();
+		String[] items = new String[inventorynumber + 1];
+		double[] prices = new double[inventorynumber + 1];
+		int[] numberOfItemsBought = new int[inventorynumber];
+		int numberOfCus = 0;
+		int i = 0;
+		for (i = 0; i < inventorynumber; i++); {
+			items[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		numberOfCus = scan.nextInt();
 		
+		String[] cusFirstName = new String[numberOfCus];
+		String[] cusLastName = new String[numberOfCus];
+		int itemsBought;
+		int[] people = new int[inventorynumber];
+		boolean[] isAdded = new boolean[inventorynumber];
+		
+		for (int j = 0; j < numberOfCus; j++) {
+			cusFirstName[j] = scan.next();
+			cusLastName[j] = scan.next();
+			itemsBought = scan.nextInt();
+			for (int k = 0; k < itemsBought; k++) {
+				int tempNumber = scan.nextInt();
+				String tempItem = scan.next();
+				if (tempItem.contentEquals(items[k])) {
+					if (isAdded[k] == false) {
+						people[k]++;
+					}
+					numberOfItemsBought[k] = numberOfItemsBought[k] + tempNumber;
+					people[k]++;
+					isAdded[k] = true;
+				}
+			}
+		}
+	for (int f = 0; f < inventorynumber; f++) {
+		if (people[f] == 0) {
+			System.out.println("No customers bought " + items[f]);
+		}
+		else {
+			System.out.println(people[f] + "customers bought " + numberOfItemsBought[f] + " " + items[f]);
+		}
+	}
+	scan.close();
 	}
 }
